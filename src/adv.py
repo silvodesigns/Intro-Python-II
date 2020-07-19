d9edb97a78d35aea9d86795eb55ad05f85683265
Kevin Silvestre <kevsilvo@gmail.com>
2020-07-19 16:51:33 -0400

    movement around maze now possible

--- src/adv.py
+++ src/adv.py
@@ -42,16 +42,57 @@ room['treasure'].s_to = room['narrow']
 # Make a new player object that is currently in the 'outside' room.
 
 myPlayer = Player("kevin", room["outside"])
-
+direction = ""
 # Write a loop that:
-while(???):
+while(direction != "quit"):
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
-print(myPlayer)
+    print(myPlayer)
 # * Waits for user input and decides what to do.
-#
+    print("N = go north, S = go south, E = go east, W = go west")
+    direction = input("Where do you wanna go now?")
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
+    if(direction == "n" and myPlayer.location.name == "Outside Cave Entrance"):
+        myPlayer.location = room["foyer"]
+    elif(direction == "w" or direction == "e" or direction == "s"
+        and myPlayer.location.name == "Outside Cave Entrance" \
+        ):
+        print("Sorry you have hit a wall, nothing in that direction...")
+
+    elif(myPlayer.location.name == "Foyer" and direction == "n"):
+        myPlayer.location = room["overlook"]
+    elif(myPlayer.location.name == "Foyer" and direction == "e"):
+        myPlayer.location = room["narrow"]
+    elif(myPlayer.location.name == "Foyer" and direction == "w" ):
+        print("Sorry you have hit a wall, nothing in that direction...")
+    elif(myPlayer.location.name == "Foyer" and direction == "s"):
+        myPlayer.location = room["outside"]
+    
+    elif(myPlayer.location.name == "Grand Overlook" and direction == "s"):
+        myPlayer.location.name = room["foyer"]
+    elif(myPlayer.location.name == "Grand Overlook" and direction == "n" \
+        or direction == "e" or direction ==  "w"):
+         print("Sorry you have hit a wall, nothing in that direction...")
+
+    elif(myPlayer.location.name == "Narrow Passage" and direction == "n"):
+        myPlayer.location.name = room["treasure"]
+    elif(myPlayer.location.name == "Narrow Passage" and direction == "s" \
+        or direction == "e"):
+        print("Sorry you have hit a wall, nothing in that direction...")
+    elif(myPlayer.location.name == "Narrow Passage" and direction == "w"):
+        myPlayer.location.name == "foyer"
+
+    elif(myPlayer.location.name == "Treasure Chamber" and direction == "s"):
+        myPlayer.location.name = room["narrow"]
+    elif(myPlayer.location.name == "Treasure Chamber" and direction == "n"\
+        or direction == "w" or direction == "e"):
+        print("Sorry you have hit a wall, nothing in that direction...")
+
+
+
+
+
 # If the user enters "q", quit the game.
